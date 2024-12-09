20a60ebebcad36d6fb166a5921d0e46544370061
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-09 23:15:32 +0100

    feat: pari steps (not functions)

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -7,17 +7,37 @@ Dichiariamo chi ha vinto.
 */
 
 // 1. chiede all'utente di scegliere pari o dispari
+const userChoice = prompt("Sceglie pari o dispari");
 
 // 2. chiede all'utente di inserire un numero da 1 a 5
+const userNumber = Number(prompt("Sceglie un numero tra 1 e 5"));
 
 // 3. genera un numero random (da 1 a 5) per il computer (usando una funzione)
+const computerNumber = Math.floor(Math.random() * 5) + 1;
 
 // 4. stampa il numero del computer nel console
+console.log(computerNumber)
 
 // 5. somma i due numeri 
+const sum = userNumber + computerNumber;
 
 // 6. stampa la somma dei due numeri nel console
+console.log(sum)
 
 // 7. stabila se la somma dei due numeri è pari o dispari (usando una funzione)
 
+let evenOrOdd = ""
+
+if(sum % 2 === 0) {
+    evenOrOdd = "pari"
+} else {
+    evenOrOdd = "dispari"
+}
+
 // 8. dichiara chi ha vinto : se la risposta del punto 7 è uguale alla scelta dell'utente, ha vinto il utente; altrimenti ha vinto il computer 
+
+if(evenOrOdd === userChoice) {
+    alert("Hai vinto!")
+} else {
+    alert("Hai perso")
+}
