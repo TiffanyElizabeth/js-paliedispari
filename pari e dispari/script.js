a66969bb49177b2d6b0f98c673d3360a5a24f7a9
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-09 21:23:43 +0100

    feat: pari steps breakdown

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -6,3 +6,18 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
+// 1. chiede all'utente di scegliere pari o dispari
+
+// 2. chiede all'utente di inserire un numero da 1 a 5
+
+// 3. genera un numero random (da 1 a 5) per il computer (usando una funzione)
+
+// 4. stampa il numero del computer nel console
+
+// 5. somma i due numeri 
+
+// 6. stampa la somma dei due numeri nel console
+
+// 7. stabila se la somma dei due numeri è pari o dispari (usando una funzione)
+
+// 8. dichiara chi ha vinto : se la risposta del punto 7 è uguale alla scelta dell'utente, ha vinto il utente; altrimenti ha vinto il computer 
