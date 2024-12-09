1c60b22749825efb36b077c73ca5d7ab9da16a74
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-09 23:35:26 +0100

    feat: pari add functions

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -13,7 +13,18 @@ const userChoice = prompt("Sceglie pari o dispari");
 const userNumber = Number(prompt("Sceglie un numero tra 1 e 5"));
 
 // 3. genera un numero random (da 1 a 5) per il computer (usando una funzione)
-const computerNumber = Math.floor(Math.random() * 5) + 1;
+/**
+ * Returns a random number between min & max (inclusive)
+ * 
+ * @param {number} min - min number
+ * @param {number} max - max number
+ * @returns {number}
+ */
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const computerNumber = getRndInteger(1, 5);
 
 // 4. stampa il numero del computer nel console
 console.log(computerNumber)
@@ -25,19 +36,39 @@ const sum = userNumber + computerNumber;
 console.log(sum)
 
 // 7. stabila se la somma dei due numeri è pari o dispari (usando una funzione)
+/**
+ * Check if a given number is even or odd
+ * 
+ * @param {number} num
+ * @returns {boolean}
+ */
+function isEven(num) {
+    if(num % 2 === 0) {
+        return true;
+    }
+    return false;
+}
 
-let evenOrOdd = ""
+if((isEven(sum) && userChoice === "pari") || (!isEven(sum) && userChoice === "dispari")) {
+    alert ("Hai vinto!")
+} else {
+    alert("Hai perso")
+}
+
+/*let evenOrOdd = ""
 
 if(sum % 2 === 0) {
     evenOrOdd = "pari"
 } else {
     evenOrOdd = "dispari"
 }
+*/
 
 // 8. dichiara chi ha vinto : se la risposta del punto 7 è uguale alla scelta dell'utente, ha vinto il utente; altrimenti ha vinto il computer 
 
-if(evenOrOdd === userChoice) {
+/*if(evenOrOdd === userChoice) {
     alert("Hai vinto!")
 } else {
     alert("Hai perso")
 }
+*/
