05971860e8e99ef6686e923e9e417a6de9150ad1
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-09 21:48:45 +0100

    feat: palindroma complete steps

--- palidroma/script.js
+++ palidroma/script.js
@@ -4,7 +4,23 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 // 1. chiede all'utente di inserire una parola (prompt)
+const userWord = prompt("Inserisca una parola"); 
 
 // 2. crea una funzione per capire se la parola inserita è palindroma (baab) (loop)
-
-        // informa l'utente se la parola è palindroma o no 
+/**
+ * Check if a given word is a palindrome or not
+ * 
+ * @param {string} word - given word 
+ * @returns {boolean} - true = palindrome
+ */
+function isPalindrome(word) {
+    let wordBackwards="";
+    for(let i=userWord.length - 1; i >= 0; i--) {
+        wordBackwards = wordBackwards + userWord[i];
+    }
+    if(userWord === wordBackwards) {
+        alert("La parola è palindroma")
+    } else {
+        alert("La parola non è palindroma")
+    }
+}
