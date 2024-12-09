79c27639208137696724752dbc1e198343ea07e8
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-09 21:40:50 +0100

    feat: palindroma breakdown steps

--- palidroma/script.js
+++ palidroma/script.js
@@ -3,4 +3,8 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// 1. 
+// 1. chiede all'utente di inserire una parola (prompt)
+
+// 2. crea una funzione per capire se la parola inserita è palindroma (baab) (loop)
+
+        // informa l'utente se la parola è palindroma o no 
